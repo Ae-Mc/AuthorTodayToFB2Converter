159c56a8e57e417a0c7b7cd5800e1d001fbbe53e
Ae-Mc <ae_mc@mail.ru>
2020-09-01 19:26:52 +0300

    Authorization now works!

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
-from requests import Session
-from bs4 import BeautifulSoup
-from os import path, mkdir
+from dataclasses import dataclass
+from os import mkdir, path
 
+from bs4 import BeautifulSoup
+from requests import Response, Session
 
 OutputFolder = "Output"
 try:
@@ -9,24 +10,29 @@ try:
 except FileExistsError:
     pass
 
+USERNAME = "<username>"
 
+
+@dataclass
 class Pages:
     main = "https://author.today"
-    login = main + "/account/login"
-    personalAccount = main + "/account/my-page"
-    purchased = main + "/u/<username>/library/purchased"
+    login = f"{main}/account/login"
+    personalAccount = f"{main}/account/my-page"
+    profile = f"{main}/u/{USERNAME}"
+    purchased = f"{main}/u/{USERNAME}/library/purchased"
 
 
 def GetBookFirstChapter(url: str, session: Session):
     with session.get(url) as reader:
         print(reader.url)
-        with open(path.join(OutputFolder,
-                            url[url.find("reader") + 7:] + ".php"),
-                  "w") as bookFile:
-            bookFile.write(reader.text)
 
 
-with Session() as session:
+def GetRequestVerificationToken(response: Response) -> str:
+    DOM: BeautifulSoup = BeautifulSoup(response.text, "html.parser")
+    return DOM.select_one("form#logoffForm > input").attrs["value"]
+
+
+def Authorize(session: Session) -> bool:
     with open("./PrivateConfig/mainPassword.txt") as f:
         password = f.readlines()[0]
     with open("./PrivateConfig/mail.ru-email.txt") as f:
@@ -35,30 +41,34 @@ with Session() as session:
         "Login": email,
         "Password": password
     }
-
+    requestVerificationToken = GetRequestVerificationToken(
+        session.get(Pages.login))
+    session.headers["__RequestVerificationToken"] = requestVerificationToken
     with session.post(Pages.login, data) as loginResponse:
-        # print(loginResponse.text)
-        pass
-    with session.get(Pages.personalAccount) as personalAccountResponse:
-        with open(path.join(OutputFolder, "personalAccount.php"),
-                  "w") as personalAccountFile:
-            personalAccountFile.write(personalAccountResponse.text)
+        if loginResponse.status_code != 200:
+            print(
+                "Error! Can't log in! Error code: {loginResponse.status_code}")
+            return False
+    return True
 
-    with session.get(Pages.purchased) as purchasedResponse:
-        DOM: BeautifulSoup = BeautifulSoup(purchasedResponse.text,
-                                           "html.parser")
-        bookShelf = DOM.select_one("div.book-shelf")
-        for book in bookShelf.find_all("div", attrs={"class": "bookcard"}):
-            author = book.find(
-                "h5", attrs={"class": "bookcard-authors"}).a.text
-            title = book.find(
-                "h4", attrs={"class": "bookcard-title"}).a.text
-            url = Pages.main + book.find("div", attrs={
-                "class": "thumb-buttons"}).find_all("a")[1]["href"]
-            print(f"{author}. «{title}» ({url})")
-            GetBookFirstChapter(url, session)
 
-        with open(path.join(OutputFolder,
-                            "purchased.php"),
-                  "w") as purchasedFile:
-            purchasedFile.write(purchasedResponse.text)
+with Session() as session:
+    if Authorize(session):
+        with session.get(Pages.purchased) as purchasedResponse:
+            DOM: BeautifulSoup = BeautifulSoup(purchasedResponse.text,
+                                               "html.parser")
+            bookShelf = DOM.select_one("div.book-shelf")
+            for book in bookShelf.find_all("div", attrs={"class": "bookcard"}):
+                author = book.find(
+                    "h5", attrs={"class": "bookcard-authors"}).a.text
+                title = book.find(
+                    "h4", attrs={"class": "bookcard-title"}).a.text
+                url = Pages.main + book.find("div", attrs={
+                    "class": "thumb-buttons"}).find_all("a")[1]["href"]
+                print(f"{author}. «{title}» ({url})")
+                GetBookFirstChapter(url, session)
+
+            with open(path.join(OutputFolder,
+                                "purchased.php"),
+                      "w") as purchasedFile:
+                purchasedFile.write(purchasedResponse.text)
