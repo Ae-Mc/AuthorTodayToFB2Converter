f6d2cf7b2c48923a553fdf6da079ce8104b45332
Ae-Mc <ae_mc@mail.ru>
2020-09-11 17:28:55 +0300

    Replace requests with httpx in Classes/Book.py

--- Classes/Book.py
+++ Classes/Book.py
@@ -1,19 +1,20 @@
+from httpx import Client
+
 from .BookHeader import BookHeader
-from requests import Session
 
 
 class Book():
     header: BookHeader
-    session: Session
+    client: Client
 
-    def __init__(self, session: Session = None, url: str = None):
-        if session is not None:
-            self.session = session
+    def __init__(self, client: Client = None, url: str = None):
+        if client is None:
+            self.client = Client()
         else:
-            self.session = Session()
+            self.client = client
         self.header = BookHeader()
         if url is not None:
             self.GetBookFromUrl(url)
 
     def GetBookFromUrl(self, url: str):
-        self.header.GetBookHeaderFromUrl(url, self.session)
+        self.header.GetBookHeaderFromUrl(url, self.client)
