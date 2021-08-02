a1379b7daf8087ed8dd426f2185780497d73575b
Ae-Mc <ae_mc@mail.ru>
2021-08-02 21:34:49 +0300

    Fix #9

--- Classes/Book.py
+++ Classes/Book.py
@@ -1,6 +1,7 @@
+from Classes.Functions import SearchGroupOne
 import asyncio
 from httpx import AsyncClient
-from typing import List, Tuple
+from typing import List
 
 from .BookHeader import BookHeader
 from .Chapter import Chapter
@@ -25,23 +26,45 @@ class Book:
 
     async def GetBookChapters(self) -> List[Chapter]:
         self.chapters = []
-        tasks: List[asyncio.Task] = []
         user = User("", "", await Chapter.GetUserId(self.client))
+        if len(self.header.tableOfContents) > 1:
+            await self.getMultipleChapters(user)
+        else:
+            await self.getSingleChapter(user)
+        return self.chapters
+
+    async def getMultipleChapters(self, user: User):
+        tasks: List[asyncio.Task] = []
         for chapterHeader in self.header.tableOfContents:
-            tasks.append(asyncio.create_task(self.GetBookChapter(
-                self.header.GetChapterDataUrl(chapterHeader),
-                chapterHeader,
-                user
-            )))
+            tasks.append(
+                asyncio.create_task(
+                    self.GetBookChapter(
+                        self.header.GetChapterDataUrl(chapterHeader),
+                        chapterHeader,
+                        user,
+                    )
+                )
+            )
         for task in tasks:
             await task
             self.chapters.append(task.result())
-        return self.chapters
 
-    async def GetBookChapter(self,
-                             url: str,
-                             chapterHeader: ChapterHeader,
-                             user: User) -> Tuple[Chapter, int]:
+    async def getSingleChapter(self, user: User):
+        readerPage = await self.client.get(self.header.GetReaderUrl())
+        readerPage.raise_for_status()
+        chapterId = int(SearchGroupOne(r"chapterId: (\d+),", readerPage.text))
+        chapterHeader = ChapterHeader(self.header.title, chapterId)
+        self.chapters.append(
+            await self.GetBookChapter(
+                self.header.GetChapterDataUrl(chapterHeader),
+                chapterHeader,
+                user,
+            )
+        )
+
+    async def GetBookChapter(
+        self, url: str, chapterHeader: ChapterHeader, user: User
+    ) -> Chapter:
         chapter = Chapter(chapterHeader, self.client, user)
         await chapter.GetChapterFromUrl(url)
         return chapter
